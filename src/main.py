fb766d0387a2798b6c63b33d9b5517add602b8a2
Ondřej Kutáček <ondrakut@seznam.cz>
2023-04-19 13:21:04 +0200

    sloučení proměnných do listů

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 follower_x = random.randint(50 , 1200) 
 follower_y = random.randint(50, 600)
-rychlost = 1
+rychlost = 2
 ucitel_x = 200
-ucitel_y = 690
-
+ucitel_y = 720
+start_ucitel = (170,660)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,26 +24,26 @@ while True:
         elif ucitel_y < follower_y:
             ucitel_y += rychlost
     if distance <= 60:
-        if ucitel_x < 200 - 30:
+        # Move učitele
+        if ucitel_x < start_ucitel[0]:
             ucitel_x += rychlost
-        if ucitel_x > 200 - 30:
+        if ucitel_x > start_ucitel[0]:
             ucitel_x -= rychlost
-        if ucitel_y > 690 - 30:
+        if ucitel_y > start_ucitel[1]:
             ucitel_y += rychlost
-        if ucitel_y < 690 - 30:
+        if ucitel_y < start_ucitel[1]:
             ucitel_y += rychlost
-        if follower_x < 200 - 30:
+        # Posouvani vedenýho folowera
+        if follower_x < start_ucitel[0]:
             follower_x += rychlost
-        if follower_x > 200 - 30:
+        if follower_x > start_ucitel[0]:
             follower_x -= rychlost
-        if follower_y < 690 - 30:
+        if follower_y < start_ucitel[1]:
             follower_y += rychlost
-        if follower_y > 690 - 30:
+        if follower_y > start_ucitel[1]:
             follower_y += rychlost
-    if follower_x == 170 and follower_y == 660:
-        follower_x += rychlost
-        follower_y += rychlost
-        
+
+      
     screen.fill((255,255,255))
     pygame.draw.circle(screen, (0,0,0), (follower_x, follower_y), 30)
     pygame.draw.circle(screen, (255,0,0), (ucitel_x, ucitel_y), 30)
