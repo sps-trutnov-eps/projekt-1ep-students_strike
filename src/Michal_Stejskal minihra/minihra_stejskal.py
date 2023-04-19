c2b6144aed9c11b20aab6fae8da2f1caca77dc56
Michal Stejskal <stejskalm22@spstrutnov.cz>
2023-04-19 20:24:55 +0200

    Začátek minihry - Michal Stejskal

---
+++ src/Michal_Stejskal minihra/minihra_stejskal.py
@@ -0,0 +1,28 @@
+import pygame
+import sys 
+import random
+
+pygame.init()
+
+cerna_barva = (0, 0, 0)
+bila_barva = (255, 255, 255)
+cervena_barva = (255, 0, 0)
+modra_barva = (0, 0, 255)
+zelena_barva = (0, 255, 0)
+
+
+
+
+
+pygame.display.set_caption("minihra") 
+screen = pygame.display.set_mode((800, 600))
+
+
+
+
+
+
+
+
+
+pygame.display.update()
