f7ae0e78be63b48cd6f9c49349388a929255888e
Jakub Šenkýř <senkyr@spstrutnov.cz>
2023-04-19 13:10:58 +0200

    Data v samostatných seznamech

--- src/main.py
+++ src/main.py
@@ -6,14 +6,11 @@ okno = pygame.display.set_mode((800, 600))
 
 radius = 25
 
-c0_x = random.randint(radius, 800 - radius)
-c0_y = random.randint(radius, 600 - radius)
-
-c1_x = random.randint(radius, 800 - radius)
-c1_y = random.randint(radius, 600 - radius)
-
-c2_x = random.randint(radius, 800 - radius)
-c2_y = random.randint(radius, 600 - radius)
+x_list = []
+y_list = []
+for i in range(3):
+    x_list.append(random.randint(radius, 800 - radius))
+    y_list.append(random.randint(radius, 600 - radius))
 
 while True:
     for udalost in pygame.event.get():
@@ -26,9 +23,8 @@ while True:
     
     okno.fill((255, 255, 255))
     
-    pygame.draw.circle(okno, (0, 0, 0), (c0_x, c0_y), radius)
-    pygame.draw.circle(okno, (0, 0, 0), (c1_x, c1_y), radius)
-    pygame.draw.circle(okno, (0, 0, 0), (c2_x, c2_y), radius)
+    for i in range(3):
+        pygame.draw.circle(okno, (0, 0, 0), (x_list[i], y_list[i]), radius)
     
     pygame.display.update()
 
