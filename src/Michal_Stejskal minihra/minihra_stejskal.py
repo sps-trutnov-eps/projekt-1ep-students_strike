eab20fa1f6a2d7eeee955a2f4ef142d23baa4ad4
Michal Stejskal <stejskalm22@spstrutnov.cz>
2023-04-29 16:28:41 +0200

    plocha

--- src/Michal_Stejskal minihra/minihra_stejskal.py
+++ src/Michal_Stejskal minihra/minihra_stejskal.py
@@ -2,6 +2,7 @@ import pygame
 import sys 
 import random
 
+
 pygame.init()
 
 cerna_barva = (0, 0, 0)
@@ -11,18 +12,38 @@ modra_barva = (0, 0, 255)
 zelena_barva = (0, 255, 0)
 
 
+             
+    
+
+okno = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("minihra")
 
 
+done = False
+clock = pygame.time.Clock()
 
-pygame.display.set_caption("minihra") 
-screen = pygame.display.set_mode((800, 600))
+while not done:
+    
+    clock.tick(60)
+
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT: 
+            done = True  
+
+    
+    okno.fill(modra_barva)
+
+   
+    
+
+    pygame.draw.rect(okno, (cervena_barva), [350, 580, 50, 20])
+
+   
+   
+
+    
+    pygame.display.flip()
 
 
+pygame.quit()
 
-
-
-
-
-
-
-pygame.display.update()
