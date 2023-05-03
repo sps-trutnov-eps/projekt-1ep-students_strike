ca9fc2ceade58a053180b77893e99f98f5d1b18d
Václav Fišar <fisarv22@spstrutnov.cz>
2023-05-03 13:19:20 +0200

    minigame ( escape room )
    updated grafika, počátek itemů jinak nefunkční

--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ import sys
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((1820, 980))
+screen = pygame.display.set_mode((1280, 720))
 FPS = 60
+# images
 background = pygame.image.load('třída.png')
-background = pygame.transform.scale(background, (1820, 980))
-
+background = pygame.transform.scale(background, (1280, 720))
+staple = pygame.image.load('staple.png')
+staple = pygame.transform.scale(staple, (60, 60))
+compasses = pygame.image.load('compasses.png')
+compasses = pygame.transform.scale(compasses, (60, 60))
 font = pygame.font.SysFont('Consolas', 50)
 game = True
 while game:
@@ -17,10 +21,19 @@ while game:
             pygame.quit()
             sys.exit()
     
+    
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
         sys.exit()
+    
+    #if collide mouse idk s staple:
+        #screen.blit(staple,(1150,660))
+    #if collide mouse idk s staple:
+        #screen.blit(compasses,(1150,660))
+        
         
     screen.blit(background,(0,0))
+    screen.blit(compasses,(1050,660))
+    screen.blit(staple,(1150,660))
     pygame.display.update()
