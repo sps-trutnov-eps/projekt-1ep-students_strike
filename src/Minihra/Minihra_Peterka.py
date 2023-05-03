f4ed251175d42daa67b1772e1a68fb75f896df68
Adam Peterka <peterkaa22@spstrutnov.cz>
2023-05-03 13:19:15 +0200

    timer

--- src/Minihra/Minihra_Peterka.py
+++ src/Minihra/Minihra_Peterka.py
@@ -9,7 +9,7 @@ w = 1280
 h = 720
 rychlost = 3
  
-
+font = pygame.font.SysFont("arialblack", 80)
 
 screen =pygame.display.set_mode((w, h))
 pygame.display.set_caption("minihra")
@@ -32,14 +32,26 @@ enemy1_postoj_rect = enemy1_postoj.get_rect()
 enemy1_postoj_rect.x = -190
 enemy1_postoj_rect.y = -15
 
+
+
+
+
+
+
+timer_event = pygame.USEREVENT + 1 
+pygame.time.set_timer(timer_event, 1000)
+time_left = 3
+
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif udalost.type == timer_event:
+            time_left -=1
     
     klavesa = pygame.key.get_pressed() 
-    if klavesa[pygame.K_SPACE]: 
+    if klavesa[pygame.K_SPACE] and hl_postava_postoj_rect.x < 1168:
         hl_postava_postoj_rect.x += rychlost
         
         
@@ -53,12 +65,15 @@ while True:
     
 
 
+   
+        
 
-
-
+    timer_text = font.render(f"{time_left}", True, (255,255,255))
+    
     screen.blit(bg, bg_rect)
     screen.blit(hl_postava_postoj, hl_postava_postoj_rect)
     screen.blit(enemy1_postoj, enemy1_postoj_rect)
+    screen.blit(timer_text, (580,5))
     
     pygame.display.update()
     
