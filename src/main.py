37171146670aed2d6165d7ee75c77ed790ec49c7
Ondřej Kutáček <ondrakut@seznam.cz>
2023-05-09 11:29:32 +0200

    Smazání starého (nepotřebného) kódu

--- src/main.py
+++ src/main.py
@@ -90,57 +90,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    distance = ((follower_x - ucitel_x) ** 2 +
-                (follower_y - ucitel_y) ** 2) ** 0.5
-    if distance > 60:
-        # Jde si pro žáka
-        if ucitel_x > follower_x:
-            ucitel_x -= rychlost
-        elif ucitel_x < follower_x:
-            ucitel_x += rychlost
-        if ucitel_y > follower_y:
-            ucitel_y -= rychlost
-        elif ucitel_y < follower_y:
-            ucitel_y += rychlost
-    else:
-        # Move učitele zpět
-        if ucitel_x < start_ucitel[0]:
-            ucitel_x += rychlost
-        if ucitel_x > start_ucitel[0]:
-            ucitel_x -= rychlost
-        if ucitel_y > start_ucitel[1]:
-            ucitel_y += rychlost
-        if ucitel_y < start_ucitel[1]:
-            ucitel_y += rychlost
-        # Posouvani vedenýho folowera
-        if not reached_start_pos:
-            flwr_start_dist = [follower_x-start_ucitel[0],
-                               follower_y-start_ucitel[1]]
-            if abs(flwr_start_dist[0]) >= rychlost:
-                if follower_x < start_ucitel[0]:
-                    follower_x += rychlost
-                if follower_x > start_ucitel[0]:
-                    follower_x -= rychlost
-            else:
-                if follower_x < start_ucitel[0]:
-                    follower_x += 1
-                if follower_x > start_ucitel[0]:
-                    follower_x -= 1
-
-            if abs(flwr_start_dist[1]) >= rychlost:
-                if follower_y < start_ucitel[1]:
-                    follower_y += rychlost
-                if follower_y > start_ucitel[1]:
-                    follower_y -= rychlost
-            else:
-                if follower_y < start_ucitel[1]:
-                    follower_y += 1
-                if follower_y > start_ucitel[1]:
-                    follower_y -= 1
-            if follower_x == start_ucitel[0] and follower_y == start_ucitel[1]:
-                reached_start_pos = True
-        if reached_start_pos == True:
-            follower_y += 1
 
     screen.fill((255, 255, 255))
     pygame.draw.circle(screen, (0, 0, 0), (follower_x, follower_y), 30)
