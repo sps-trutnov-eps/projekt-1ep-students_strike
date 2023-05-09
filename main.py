5d4ab21152e25e0801aae62378ddb78be2084605
Marek Špůr <spurm22@spstrutnov.cz>
2023-05-09 10:22:32 +0200

    Další úpravy
    - Přidána funkce pro náhodný výběr obrázku
    - Vytvoření souboru credits.txt

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 # Importování knihoven
 import pygame
 import random
+import os
 
 # Inicializace PyGame
 pygame.init()
 
+# Načítání náhodných obrázků do hry ze složky
+image = ["1.png", "2.png", "3.png", "4.png", "5.png"]
+
 # Základní parametry okna
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 720))
-pygame.image.load("src/background.png")
+pygame.display.set_caption("Killbot")
+pygame.image.load("src/pozadí.png")
 
 # Základní paramtery hry
 FPS = 120
-font = pygame.font.Font("src/digital.ttf", 30)
+font = pygame.font.Font("src/font.ttf", 30)
 run = True
+
+# Vytvoření unkce pro náhodný výběr obrázku
+def RandomImg():
+    random.choice(image)
 
 # Hlavní smyčka hry
 while run:
-    clock.tisk(FPS)
+    clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
