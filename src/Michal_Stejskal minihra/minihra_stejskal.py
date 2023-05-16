f0e0df1adf4f7608e5447c56019fb79e29fc9d39
Michal Stejskal <stejskalm22@spstrutnov.cz>
2023-05-16 19:04:38 +0200

    známky

--- src/Michal_Stejskal minihra/minihra_stejskal.py
+++ src/Michal_Stejskal minihra/minihra_stejskal.py
@@ -4,7 +4,7 @@ import random
 
 
 pygame.init()
-
+FPS = 60
 cerna_barva = (0, 0, 0)
 bila_barva = (255, 255, 255)
 cervena_barva = (255, 0, 0)
@@ -47,21 +47,43 @@ while running:
     clock.tick(60)    
      
    
-    # pohyb hráče2 
+    # pohyb hráče1 
     key = pygame.key.get_pressed() 
     if key[pygame.K_LEFT]: 
         hrac1.move(-2, 0) 
     if key[pygame.K_RIGHT]: 
         hrac1.move(2, 0) 
-     
+    if key[pygame.K_ESCAPE]:
+        pygame.quit()
+        quit()
 
     
-    okno.fill(modra_barva)
+    okno.fill(cerna_barva)
 
    
     
 
-    pygame.draw.rect(okno,(cervena_barva) , hrac1.rect)
+    pygame.draw.rect(okno,(modra_barva) , hrac1.rect)
+    
+    font1 = pygame.font.Font(None, 30)
+    znamka1 = font1.render("1", True, ((zelena_barva)))
+    okno.blit(znamka1, [100, 100])
+    
+    font2 = pygame.font.Font(None, 30)
+    znamka2 = font2.render("2", True, ((zelena_barva)))
+    okno.blit(znamka2, [200, 100])
+    
+    font3 = pygame.font.Font(None, 30)
+    znamka3 = font3.render("3", True, ((zelena_barva)))
+    okno.blit(znamka3, [300, 100])
+    
+    font4 = pygame.font.Font(None, 30)
+    znamka4 = font4.render("4", True, ((zelena_barva)))
+    okno.blit(znamka4, [400,100])
+    
+    font5 = pygame.font.Font(None, 30)
+    znamka5 = font5.render("5", True, ((zelena_barva)))
+    okno.blit(znamka5, [500, 100])
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -74,7 +96,7 @@ while running:
     pygame.display.update()       
     
      
-clock.tick(60) 
+clock.tick(FPS) 
 
 pygame.quit()
 
