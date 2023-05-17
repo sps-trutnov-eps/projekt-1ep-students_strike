232208555707707af97c1d868a73c3d2cc37507b
Adam Peterka <peterkaa22@spstrutnov.cz>
2023-05-17 13:19:45 +0200

    pohyb

--- src/Minihra/Minihra_Peterka.py
+++ src/Minihra/Minihra_Peterka.py
@@ -7,8 +7,11 @@ clock = pygame.time.Clock()
 FPS = 60
 w = 1280
 h = 720
-rychlost = 3
- 
+rychlost = 17
+enemy_rychlost = 2
+minule_stisknuto = False 
+
+
 font = pygame.font.SysFont("arialblack", 80)
 
 screen =pygame.display.set_mode((w, h))
@@ -32,7 +35,9 @@ enemy1_postoj_rect = enemy1_postoj.get_rect()
 enemy1_postoj_rect.x = -190
 enemy1_postoj_rect.y = -15
 
-
+enemy2_postoj = pygame.image.load("enemy2.png")
+enemy2_postoj = pygame.transform.scale(enemy2_postoj, (450,250))
+enemy2_postoj_rect = enemy2_postoj.get_rect()
 
 
 
@@ -51,11 +56,23 @@ while True:
             time_left -=1
     
     klavesa = pygame.key.get_pressed() 
-    if klavesa[pygame.K_SPACE] and hl_postava_postoj_rect.x < 1168:
+    if klavesa[pygame.K_SPACE] and hl_postava_postoj_rect.x <1168 and not minule_stisknuto:
         hl_postava_postoj_rect.x += rychlost
         
+    minule_stisknuto =  klavesa[pygame.K_SPACE]
+    
+    enemy1_postoj_rect.x += enemy_rychlost
+    if enemy1_postoj_rect.x > 1009:
+        enemy1_postoj_rect.x = 1009 
+    
+    
         
         
+    
+
+    
+    
+
     
         
         
@@ -74,6 +91,8 @@ while True:
     screen.blit(hl_postava_postoj, hl_postava_postoj_rect)
     screen.blit(enemy1_postoj, enemy1_postoj_rect)
     screen.blit(timer_text, (580,5))
+    screen.blit(enemy2_postoj, enemy2_postoj_rect)
+    
     
     pygame.display.update()
     
