c3ecf3785f7a4663d042bdfc8ce35c2e4244f0e2
Václav Fišar <fisarv22@spstrutnov.cz>
2023-05-17 13:12:00 +0200

    minigame
    still not funkšn

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ pygame.init()
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 720))
+black = (0,0,0)
 FPS = 60
 # images
 background = pygame.image.load('třída.png')
@@ -13,6 +14,9 @@ staple = pygame.transform.scale(staple, (60, 60))
 compasses = pygame.image.load('compasses.png')
 compasses = pygame.transform.scale(compasses, (60, 60))
 font = pygame.font.SysFont('Consolas', 50)
+table1 = pygame.Rect(30, 450, 160, 80)
+table_skin = pygame.image.load('table.png')
+table_skin = pygame.transform.scale(table_skin, (160, 80))
 game = True
 while game:
     clock.tick(FPS)
@@ -21,6 +25,23 @@ while game:
             pygame.quit()
             sys.exit()
     
+    screen.blit(background,(0,0))
+    pygame.draw.rect(screen, black, table1)
+    screen.blit(compasses,(1150,660))
+    screen.blit(staple,(1215,660))
+    
+    mx = pygame.mouse.get_pos()[0]
+    my = pygame.mouse.get_pos()[1]
+    print(mx, my)
+    mouse = pygame.mouse.get_pressed()
+    if mouse[0]:
+        if 
+            screen.blit(table_skin, (30, 450))
+        
+        
+    
+    
+        
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
@@ -33,7 +54,5 @@ while game:
         #screen.blit(compasses,(1150,660))
         
         
-    screen.blit(background,(0,0))
-    screen.blit(compasses,(1050,660))
-    screen.blit(staple,(1150,660))
+    
     pygame.display.update()
