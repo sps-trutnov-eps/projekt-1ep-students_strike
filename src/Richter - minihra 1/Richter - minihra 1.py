b28e861bf7c5129a7ab54aca72e433aa76b45a91
Matěj Richter <richterm22@spstrutnov.cz>
2023-05-17 13:16:51 +0200

    Start of board I quess
    I will go crazy from this one day

--- src/Richter - minihra 1/Richter - minihra 1.py
+++ src/Richter - minihra 1/Richter - minihra 1.py
@@ -1,11 +1,54 @@
 import pygame
+import sys 
+pygame.init()
 
-screen = pygame.display.set_mode((1920,1080))
+Background = pygame.image.load('pixil-frame-0.png')
 
-#loop
+
+
+
+
+
+
+#Screen
+screen = pygame.display.set_mode((1280,720))
+
+
+#window loop
 while True:
+    #Turning off the app
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           #framework shutdown
+            pygame.quit()
+            #app shutdown
+            sys.exit()
+    #alt+f4 shutdown
+    Keyboard = pygame.key.get_pressed()
+    F4 = Keyboard[pygame.K_F4]
+    if F4 == True:
+        pygame.QUIT
+    #background
+    screen.fill((255, 255, 255))
     
-    screen.fill((255,255,255))
     
+    
+    
+    
+    #Board
+    pygame.draw.rect(screen, (128, 64, 0), (390, 125, 500, 10))
+    pygame.draw.rect(screen, (128, 64, 0), (390, 450, 500, 10))
+    pygame.draw.rect(screen, (128, 64, 0), (390, 125, 10, 325))
+    pygame.draw.rect(screen, (128, 64, 0), (890, 125, 10, 335))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    pygame.display.update() 
 
 
