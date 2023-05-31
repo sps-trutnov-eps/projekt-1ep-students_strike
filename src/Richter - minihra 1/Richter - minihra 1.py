244a03c93241e484d138bb6e3ce6d7d971c0036e
Matěj Richter <richterm22@spstrutnov.cz>
2023-05-31 13:20:06 +0200

    První psaní na tabuly + počátky pozadí

--- src/Richter - minihra 1/Richter - minihra 1.py
+++ src/Richter - minihra 1/Richter - minihra 1.py
@@ -1,9 +1,10 @@
 import pygame
-import sys 
+import sys
+import random
 pygame.init()
 
-Background = pygame.image.load('pixil-frame-0.png')
 
+Popsana_tabule_1 = pygame.image.load('Popsana_tabule-1.png') 
 Clean_board_colour = (0, 102, 0) 
 
 
@@ -19,6 +20,9 @@ pygame.draw.rect(screen, (128, 64, 0), (390, 125, 10, 325))
 pygame.draw.rect(screen, (128, 64, 0), (890, 125, 10, 335))
 pygame.draw.rect(screen, (98, 149, 101), (400, 135, 490, 315))
 
+Drawing_on_board = 1
+if Drawing_on_board == 1:
+    screen.blit(Popsana_tabule_1, (440, 175)) 
 
 
 
@@ -37,11 +41,6 @@ while True:
     F4 = Keyboard[pygame.K_F4]
     if F4 == True:
         pygame.QUIT
-    #background
-
-    
-    
-    
     
   
     
@@ -51,7 +50,7 @@ while True:
         mpos = pygame.mouse.get_pos()
         Clean_circle_xpos = mpos[0] - 40
         Clean_circle_ypos = mpos[1] - 40
-        if 440 < mpos[0] < 850 and 155 < mpos[1] < 450:
+        if 440 < mpos[0] < 850 and 175 < mpos[1] < 430:
             pygame.draw.ellipse(screen, Clean_board_colour, (Clean_circle_xpos, Clean_circle_ypos, 80, 80)) 
            
     
